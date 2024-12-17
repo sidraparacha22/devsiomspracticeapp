44d112f746178c22e80e6feff6b2af3bedb973dc
sidra20 <sidraparacha23@gmail.com>
2024-12-17 23:30:03 +0500

    onboarding screen 2 and 3 and splash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.intuit.sdp:sdp-android:1.1.1")
+    implementation("com.intuit.ssp:ssp-android:1.1.1")
+
 }
---
+++ app/src/main/java/com/practicetask/practiceapp/activities/OnBoardingActivity.kt
@@ -0,0 +1,17 @@
+package com.practicetask.practiceapp.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.practicetask.practiceapp.R
+
+class OnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_onboarding)
+
+    }
+}
