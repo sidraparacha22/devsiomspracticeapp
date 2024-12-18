8f2b08faa95bcdd9d580e82f6d7da527b53026c6
sidra20 <sidraparacha23@gmail.com>
2024-12-18 21:58:41 +0500

    edited

--- app/src/main/java/com/practicetask/practiceapp/activities/SplashActivity.kt
+++ app/src/main/java/com/practicetask/practiceapp/activities/SplashActivity.kt
@@ -13,7 +13,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.onboarding_screen1)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
